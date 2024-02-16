57e6773c2efe6111bc60af94b84acc6fa33b34fa
ahadley1124 <admin@austinh.dev>
2024-02-16 07:34:53 -0500

    Add Template File
    Signed-off-by: ahadley1124 <admin@austinh.dev>

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('DOM loaded with JavaScript');
 });
+
+let screen_width = window.innerWidth;
+let header_offset = screen_width - 544;
+
+if (screen_width > 544) {
+    //get the container class, remove the previous margin-left percentage and add the new margin-left pixel value
+    document.getElementById("header").style.left = header_offset + "px";
+} else {
+    document.getElementById("header").style.left = "0px";
+}
